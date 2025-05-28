0857e8e5f877e7899a05d7374becb7942acf4ecc
Casper Coder <zagrodzinski@gmail.com>
2025-05-28 16:53:36 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
+app.listen(port, () => {
+  console.log(`Backend server is running at http://localhost:${port}`);
+});
